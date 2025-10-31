d1d84c8465a3c0a05f03264174d014c433100dfa
aziz-aitbihi22 <azizbihi2003@gmail.com>
2025-10-31 16:52:24 +0100

    Ajout fonction afficher_version

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-#!/ usr/ bin / env python 3 # -*- coding : utf -8 -*-
+#!/ usr/ bin / env python 3 
+# # -*- coding : utf -8 -*-
 
 def main ():
     print(" Bonjour␣Git!")
+
 if __name__ == "__main__":
     main ()
